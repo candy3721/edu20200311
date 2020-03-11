56833a5823000b03e4c41760f0b9f211363bee63
candy3721 <402404932@qq.com>
2020-03-11 16:15:28 +0800

    temp1

--- database/migrations/2020_03_11_155252_create_manager_table.php
+++ database/migrations/2020_03_11_155252_create_manager_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateManagerTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('manager', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('username',20)->notNull();
+            $table->string('password')->notNull();
+            $table->enum('gender',[1,2])->notNull()->default('1')； //性别，1=男，2=女
+            $table->string('mobile',11);
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('manager');
     }
 }
